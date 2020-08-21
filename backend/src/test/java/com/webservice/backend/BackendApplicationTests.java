38464ea356d7770fce9d1d51f7d214af63c39ee1
jvnascimento <nascimento783@gmail.com>
2020-08-20 21:27:36 -0300

    projetos iniciais do frontend e backend

---
+++ backend/src/test/java/com/webservice/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
