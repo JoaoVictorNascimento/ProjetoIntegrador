8c8ae950eedab766bd6d782d67c212b94c95f0a2
João Victor Nascimento <joao_victorxy@hotmail.com>
2020-08-30 17:23:45 -0300

    feita a autenticação jwt e geracao de token

---
+++ backend/src/main/java/com/webservice/backend/security/autentication/model/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.webservice.backend.security.autentication.model;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
